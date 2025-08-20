4175420761dcdb2c495c5f3e4ac54b9a40792663
Denizbaranaykul <daykul75@gmail.com>
2025-08-20 10:55:37 +0300

    slider + hakkındanın başı

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Hello!');
+const slides = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let index = 0;
+
+function showSlide(i) {
+  if (i < 0) {
+    index = images.length - 1;
+  } else if (i >= images.length) {
+    index = 0;
+  } else {
+    index = i;
+  }
+  slides.style.transform = `translateX(${-index * 100}%)`;
+}
+
+
+// Otomatik geçiş (3 sn)
+setInterval(() => {
+  showSlide(index + 1);
+}, 9000);
+
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
