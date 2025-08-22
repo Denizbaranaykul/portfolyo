4e10f13e02dfd068c43363acb3558cd634907fea
haaland <139878155+Denizbaranaykul@users.noreply.github.com>
2025-08-22 14:20:00 +0300

    Slider'ı normal arka plana çevir

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-const slides = document.querySelector(".slides");
-const images = document.querySelectorAll(".slides img");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-
-let index = 0;
-
-function showSlide(i) {
-  if (i < 0) {
-    index = images.length - 1;
-  } else if (i >= images.length) {
-    index = 0;
-  } else {
-    index = i;
-  }
-  slides.style.transform = `translateX(${-index * 100}%)`;
-}
-
-
-// Otomatik geçiş (3 sn)
-setInterval(() => {
-  showSlide(index + 1);
-}, 9000);
-
-
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
